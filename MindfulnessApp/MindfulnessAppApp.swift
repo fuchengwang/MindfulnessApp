39595e599a2260966b490ec658e56a3a20f46f87
真的是你吗阿诚 <wynfrith@outlook.com>
2026-02-09 16:15:36 +0800

    Initial Commit

---
+++ MindfulnessApp/MindfulnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindfulnessAppApp.swift
+//  MindfulnessApp
+//
+//  Created by 富诚 on 2026/2/9.
+//
+
+import SwiftUI
+
+@main
+struct MindfulnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
